a0be523bdebd84bfd2e9c393fcb5a9d41199cf3c
Scott Sullivan <scottmsul@gmail.com>
2026-03-03 19:59:15 -0500

    added logging for URCL (motor logging) and advantagekit logger

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,19 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.urcl.URCL;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
  * the TimedRobot documentation. If you change the name of this class or the package after creating
  * this project, you must also update the Main.java file in the project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
@@ -23,6 +26,15 @@ public class Robot extends TimedRobot {
      * initialization code.
      */
     public Robot() {
+        // adds advantagekit data to network tables
+        Logger.addDataReceiver(new NT4Publisher());
+
+        // for advantagekit
+        Logger.start();
+
+        // logs motor data
+        URCL.start();
+
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
